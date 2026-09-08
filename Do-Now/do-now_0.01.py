b9defa8f95791a7d9c3ca1e5b85157d2859e9db1
Jerry (Jared) Price <jaredmillerprice@gmail.com>
2026-09-08 14:40:25 -0700

    updates

---
+++ Do-Now/do-now_0.01.py
@@ -0,0 +1,30 @@
+#Do-Now 0.1
+
+#Overview
+#This Do-Now will help intro basic usability of Notepad++ with Python
+
+#Task 1: Run this file!
+#To run this file from Notepad++, using the following steps
+# 1. Alt + F (opens the file menu)
+# 2. F (opens containing folder
+# 3. Down arrow to 'cmd', hit enter
+# 4. In the command prompt, type the following:
+#   a. python do-now_0.01.py
+#   b. what does your program output?
+
+#Task 2: Adding to this file!
+#In the line below, let's type a line of code to have our program run!
+
+#What did your line of code output in the command prompt?
+#Welcome to the world of coding!
+
+#Task 3: What is an error?
+#On the next line, uncomment the line of code and run the program.
+# print(hello, world!)
+#What happend? What kind of error did the console give you?
+
+#Takeaways for today
+# 1. To run a code, make sure to follow our keystroke steps
+# 2. To call a program in the console, we always start with python, then the entire name of our file!
+# 3. To comment or uncomment something, use the key command Ctrl + Q
+# 4. Python is sensitive to the way we input information! Make sure to pay attention to syntax!
