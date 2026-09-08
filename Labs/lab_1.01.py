8432f60a9394a36234e9c71b50498243d2fb713c
Jerry (Jared) Price <jaredmillerprice@gmail.com>
2026-09-08 13:38:03 -0700

    add lab.101.py to GitHub

---
+++ Labs/lab_1.01.py
@@ -0,0 +1,34 @@
+#Name:
+#Lab 1.01 - Predicting Ouputs
+
+#Overview
+#In this lab, you will be using the print() function, as we practiced in the Do-Now. In this lab, you will practice using the print() function to predict the output of various prompts.
+
+#Instructions
+#Use the following template to test each input for print() and predict the output. After you get the output, reflect on the output.
+
+#Template (to copy and paste; you will do this 16 times total in the lab!)
+#Each template can be ordered by letter or number
+
+#Input:
+#Prediction: (what data type?) along with value
+#Output:
+#Reflection:
+
+#Inputs
+# a. 10 * 2
+# b. .5 * 2
+# c. 10/2
+# d. 10%2
+# e. 2 ** 3
+# f. (2+5)*3
+# g. 2 + 5 * 3
+# h. 'ab' + '12' + '3'
+# i. x
+# j. "ab" + "cd"
+# k. 'abc' * 2
+# l. '1' * 2 + '2' * 3
+# m. 1 * 2 + '3' * 2
+# n. 'A' ** 2
+# o. 'bc' % 2
+# p. 'bc' / 2
